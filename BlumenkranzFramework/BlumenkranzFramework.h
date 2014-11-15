96922b04c324952faac97769adf3e568ea8955b5
Andrey Panfilov <uncured@gmail.com>
2014-11-16 00:41:07 +0300

    Framework target
    Changed synthetic framework target with original Cocoa Touch framework
    for iOS 8+

---
+++ BlumenkranzFramework/BlumenkranzFramework.h
@@ -0,0 +1,8 @@
+//! Project version number for BlumenkranzFramework.
+FOUNDATION_EXPORT double BlumenkranzFrameworkVersionNumber;
+
+//! Project version string for BlumenkranzFramework.
+FOUNDATION_EXPORT const unsigned char BlumenkranzFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BlumenkranzFramework/PublicHeader.h>
+#import "Blumenkranz.h"
